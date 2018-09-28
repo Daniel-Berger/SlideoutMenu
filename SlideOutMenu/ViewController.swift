f993f4b49f931806cb844f327e89f889d3fa0cdf
Daniel-Berger <doniberger@gmail.com>
2018-09-27 23:21:49 -0400

    add tableview

--- SlideOutMenu/ViewController.swift
+++ SlideOutMenu/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.backgroundColor = UIColor.lightGray
+        setupNavigationItem()
+        
     }
-
+    
+    fileprivate func setupNavigationItem() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        cell.textLabel?.text = "Row: \(indexPath.row)"
+        return cell
+    }
+    
+    
+    
+    @objc func handleOpen() {
+        print("Opening menu...")
+    }
+    
+    @objc func handleHide() {
+        print("Hiding menu...")
+    }
 
 }
 
