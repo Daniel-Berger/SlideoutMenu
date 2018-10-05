76d7ca43065470da3d95d00e5e14d6065b463f11
Daniel-Berger <doniberger@gmail.com>
2018-10-05 13:08:01 -0400

    added attributed string

---
+++ SlideOutMenu/Views/CustomMenuHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomMenuHeaderView.swift
+//  SlideOutMenu
+//
+//  Created by dberger1 on 10/5/18.
+//  Copyright © 2018 dberger1. All rights reserved.
+//
+
+import UIKit
+
+class CustomMenuHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SlideOutMenu/Views/SpacerView.swift
@@ -0,0 +1,9 @@
+//
+//  SpacerView.swift
+//  SlideOutMenu
+//
+//  Created by dberger1 on 10/5/18.
+//  Copyright © 2018 dberger1. All rights reserved.
+//
+
+import Foundation
